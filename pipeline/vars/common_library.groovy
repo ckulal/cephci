90908424d5119c4ddc6fc0173e53b8390734fb85
ckulal <83442614+ckulal@users.noreply.github.com>
2021-08-24 17:37:20 +0530

    Update common_library.groovy

--- pipeline/vars/common_library.groovy
+++ pipeline/vars/common_library.groovy
@@ -10,6 +10,11 @@ def jsonToMap(def jsonFile) {
     /*
         Read the JSON file and returns a map object
     */
+    def FileExists = sh (returnStatus: true, script: "ls -l ${jsonFile}")
+    if (tier0FileExists != 0) {
+        println "File {jsonFile} does not exist."
+        return [:]
+    }
     def props = readJSON file: jsonFile
     return props
 }
@@ -20,42 +25,50 @@ def getCIMessageMap() {
     */
     def ciMessage = "${params.CI_MESSAGE}" ?: ""
     if (! ciMessage?.trim() ) {
-        error "The CI_MESSAGE has not been provided"
+        return [:]
     }
-
     def compose = readJSON text: "${params.CI_MESSAGE}"
-
     return compose
 }
 
 def fetchMajorMinorOSVersion(def build_type){
+    /*
+        method accepts build_type as an input and
+        Returns RH-CEPH major version, minor version and OS platform based on build_type
+        different build_type supported: compose, osbs, cvp, rc-compose, rc-osbs
+
+    */
     def cimsg = getCIMessageMap()
     def major_ver
     def minor_ver
-    def os_ver
+    def platform
 
     if (build_type == 'compose' || build_type == 'osbs') {
         major_ver = cimsg.compose_id.substring(7,8)
         minor_ver = cimsg.compose_id.substring(9,10)
-        os_ver = cimsg.compose_id.substring(11,17).toLowerCase()
+        platform = cimsg.compose_id.substring(11,17).toLowerCase()
     }
     if (build_type == 'cvp'){
         major_ver = cimsg.artifact.brew_build_target.substring(5,6)
         minor_ver = cimsg.artifact.brew_build_target.substring(7,8)
-        os_ver = cimsg.artifact.brew_build_target.substring(9,15).toLowerCase()
+        platform = cimsg.artifact.brew_build_target.substring(9,15).toLowerCase()
     }
     if (build_type == 'rc-compose'){
         major_ver = cimsg.compose-id.substring(7,8)
         minor_ver = cimsg.compose-id.substring(9,10)
-        os_ver = cimsg.compose-id.substring(11,17).toLowerCase()
+        platform = cimsg.compose-id.substring(11,17).toLowerCase()
     }
     if (build_type == 'rc-osbs'){
         major_ver = cimsg.tag.name.substring(5,6)
         minor_ver = cimsg.tag.name.substring(7,8)
-        os_ver = cimsg.tag.name.substring(9,15).toLowerCase()
+        platform = cimsg.tag.name.substring(9,15).toLowerCase()
     }
-    return ["major_ver":major_ver, "minor_ver":minor_ver, "os_ver":os_ver]
-
+    if major_ver && minor_ver && platform{
+        return ["major_version":major_ver, "minor_version":minor_ver, "platform":platform]
+    }
+    else{
+        error "Required value is not obtained.."
+    }
 }
 
 def fetchCephVersion(def base_url){
@@ -64,5 +77,21 @@ def fetchCephVersion(def base_url){
     def document = Jsoup.connect(base_url).get().toString()
     def ceph_ver = document.findAll(/"ceph-common-([\w.-]+)\.([\w.-]+)"/)[0].findAll(/([\d]+)\.([\d]+)\.([\d]+)\-([\d]+)/)
     println ceph_ver
+    if (! ceph_ver){
+        error "ceph version not found.."
+    }
     return ceph_ver[0]
+}
+
+def WriteToReleaseYaml(def major_ver, def minor_ver, def content,def lock=true){
+    lock_file_path = ${env.HOME}/lockfile.lock
+    if (! ${env.HOME}/RHCEPH-${major_ver}.${minor}.yaml){
+        sh(returnStatus: true, script: "touch ${env.HOME}/RHCEPH-${major_ver}.${minor}.yaml")
+    }
+    if (! lock_file_path){
+        def fileCreated = sh(
+        returnStatus: true, script: "touch ${lock_file_path}")
+        if
+        //write the content to a file
+    }
 }
